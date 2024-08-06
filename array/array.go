496e89f3576e20759380a9588ec159502c8ea76b
gautamarora04 <gautamvijayarora@gmail.com>
2024-08-06 18:57:11 +0530

    arrays

---
+++ array/array.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = [4]string{"Gautam", "Arora", "Gaurav"}
+
+	var price = [3]int{3, 2, 3}
+
+	fmt.Println(price)
+	fmt.Printf("%q", name) // q stands for qutation
+
+	var check [4]int
+	check[0] = 1 // access and update value
+
+	fmt.Println(len(check)) // length function
+}
